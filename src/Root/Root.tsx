a4a25c304b58707d4c708db6ce84b59cf069cad3
Ivan Kulai <daosonmail@protonmail.com>
2023-02-20 20:51:52 +0200

    - Separeted root to Root.ts - Added theme with mode: dark

---
+++ src/Root/Root.tsx
@@ -0,0 +1,19 @@
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import { Outlet } from 'react-router-dom';
+import Taskbar from './TaskBar';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+export default function Root() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Outlet />
+      <Taskbar />
+    </ThemeProvider>
+  );
+}

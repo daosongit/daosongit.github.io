7b6101d37e5ec1f6b8a3c24391ccf615459942e5
Ivan Kulai <daosonmail@protonmail.com>
2023-02-23 22:25:03 +0200

    using mui/styled instead of react component

--- src/Home/UI/StartButton.tsx
+++ src/Home/UI/StartButton.tsx
@@ -1,21 +1,14 @@
-import { Button, ButtonProps } from '@mui/material';
+import { Button, styled } from '@mui/material';
 
-export default function StartButton({ sx, onClick, children }: ButtonProps) {
-  return (
-    <Button
-      onClick={onClick}
-      sx={{
-        ...sx,
-        display: 'flex',
-        textTransform: 'none',
-        color: '#abacad',
-        border: '1px solid transparent',
-        '&:hover': {
-          borderRight: '1px solid #4e4d4d',
-          borderTop: '1px solid #757373',
-        },
-      }}>
-      {children}
-    </Button>
-  );
-}
+const StartButton = styled(Button)({
+  display: 'flex',
+  textTransform: 'none',
+  color: '#abacad',
+  border: '1px solid transparent',
+  '&:hover': {
+    borderRight: '1px solid #4e4d4d',
+    borderTop: '1px solid #757373',
+  },
+});
+
+export default StartButton;

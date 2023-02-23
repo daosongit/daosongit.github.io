fb35f0448887f17d65899e39ebc39becb20a62ba
Ivan Kulai <daosonmail@protonmail.com>
2023-02-23 22:24:11 +0200

    added powered by github api

--- src/Portfolio/Portfolio.tsx
+++ src/Portfolio/Portfolio.tsx
@@ -1,16 +1,23 @@
-import { Box, Container, Divider } from '@mui/material';
+import { Box, Container, Divider, Typography } from '@mui/material';
 import AboutMe from './About/AboutMe';
 import Header from './About/Header';
 import GitInfo from './About/GitInfo';
 import Stack from './Stack/Stack';
 import { TASKBAR_HEIGHT } from '../Root/TaskBar';
 import FirstSteps from './FirstSteps/FirstSteps';
+import { AiFillGithub as IcoGitHub } from 'react-icons/ai';
 
 export default function Portfolio() {
   return (
     <Container
       maxWidth="lg"
       sx={{ py: '20px', color: '#e2e0e0', minHeight: `calc(100vh - ${TASKBAR_HEIGHT})` }}>
+      <Box sx={{ display: 'flex', justifyContent: 'flex-end', columnGap: '5px' }}>
+        <IcoGitHub size={17} />
+        <Typography component="span" variant="body2">
+          Powered by GitHub Api
+        </Typography>
+      </Box>
       <Box component="section" sx={{ display: 'flex', justifyContent: 'space-between' }}>
         <AboutMe />
         <div>

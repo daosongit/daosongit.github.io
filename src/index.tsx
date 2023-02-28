edce4c35c753522b906f6ba93be6ac83bedeb704
Ivan Kulai <daosonmail@protonmail.com>
2023-02-28 04:26:44 +0200

    erased redux provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './Root/Router';
-import { Provider } from 'react-redux';
-// import store from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  // <Provider store={store}>
   <RouterProvider router={router} />,
-  // </Provider>,
 );

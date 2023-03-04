ed93b705c58a372e8e086bbc26b3b47c1b8e688a
Ivan Kulai <daosonmail@protonmail.com>
2023-03-04 05:28:43 +0200

    removed unused

--- src/Components/TopNavigation.tsx
+++ src/Components/TopNavigation.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, Tooltip } from '@mui/material';
-import { AiFillCloseCircle as IcoClose, AiFillGithub as IcoGitHub } from 'react-icons/ai';
+import { AiFillCloseCircle as IcoClose } from 'react-icons/ai';
 import { VscChromeMinimize as IcoMinimize } from 'react-icons/vsc';
 import { BiArrowBack as IcoBack } from 'react-icons/bi';
 import { useNavigate } from 'react-router-dom';
@@ -29,14 +29,19 @@ export default function TopNavigation() {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const tabs = useAppSelector((state) => state);
+
+  function getTabFromUrl() {
+    const href = window.location.href;
+    const path = href.slice(href.search('#') + 2);
+    return path.charAt(0).toUpperCase() + path.slice(1);
+  }
+
   const previosTab = () => {
     // todo
   };
+
   const closeTab = () => {
-    const href = window.location.href;
-    const path = href.slice(href.search('#') + 2);
-    const tab = path.charAt(0).toUpperCase() + path.slice(1);
-    dispatch(removeTab(tab));
+    dispatch(removeTab(getTabFromUrl()));
     navigate('/');
   };
 

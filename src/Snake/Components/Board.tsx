04262cf94f757eb8f0a8a8d49f34a3bf0a20e0e6
Ivan Kulai <daosonmail@protonmail.com>
2023-03-04 04:44:35 +0200

    beatify snake and food

--- src/Snake/Components/Board.tsx
+++ src/Snake/Components/Board.tsx
@@ -14,21 +14,27 @@ export default function Board({ snake, food }: BoardProps) {
       {BOARD.map((row, idxR) => (
         <Box key={idxR} display="flex">
           {row.map((cell, idxC) => {
-            const styles = { backgroundColor: 'transparent', borderRadius: '0' };
+            const styles = { background: 'transparent', borderRadius: '0' };
             const foodSx = styles;
             const snakeSx = styles;
             const isSnake = snake.find((a) => a[0] === idxR && a[1] === idxC);
             const isFood = food[0] === idxR && food[1] === idxC;
             const isHead =
               snake[snake.length - 1][0] === idxR && snake[snake.length - 1][1] === idxC;
+            const isTail = snake[0][0] === idxR && snake[0][1] === idxC;
             if (isHead) {
-              snakeSx.backgroundColor = 'white';
+              snakeSx.background = 'white';
               snakeSx.borderRadius = '30%';
+            } else if (isTail) {
+              snakeSx.background = '#e0dcdc';
+              snakeSx.borderRadius = '10%';
             } else if (isSnake) {
-              snakeSx.backgroundColor = 'white';
+              snakeSx.background = 'white';
+              snakeSx.borderRadius = '10%';
             }
             if (isFood) {
-              foodSx.backgroundColor = 'green';
+              foodSx.borderRadius = '30%';
+              snakeSx.background = 'green';
             }
             return (
               <Box

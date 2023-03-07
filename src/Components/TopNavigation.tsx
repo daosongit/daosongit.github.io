1b74ef4fc47c9702e4f558962957e25d67d51730
Ivan Kulai <daosonmail@protonmail.com>
2023-03-07 09:25:38 +0200

    deleted previos button

--- src/Components/TopNavigation.tsx
+++ src/Components/TopNavigation.tsx
@@ -1,27 +1,23 @@
 import { Box, Button, Tooltip } from '@mui/material';
 import { AiFillCloseCircle as IcoClose } from 'react-icons/ai';
 import { VscChromeMinimize as IcoMinimize } from 'react-icons/vsc';
-import { BiArrowBack as IcoBack } from 'react-icons/bi';
 import { useNavigate } from 'react-router-dom';
-import { useAppDispatch, useAppSelector } from '../redux/store';
+import { useAppDispatch } from '../redux/store';
 import { removeTab } from '../redux/tabSlice';
 import { memo } from 'react';
 
 export const TOPNAV_HEIGHT = '32px';
 
 type NavItemProps = {
-  disabled?: boolean;
   title: string;
   onClick: () => void;
   children?: React.ReactElement;
 };
 
-const NavItem = ({ children, title, onClick, disabled = false }: NavItemProps) => (
+const NavItem = ({ children, title, onClick }: NavItemProps) => (
   <Tooltip title={title} describeChild>
     <span>
-      <Button onClick={onClick} disabled={disabled}>
-        {children}
-      </Button>
+      <Button onClick={onClick}>{children}</Button>
     </span>
   </Tooltip>
 );
@@ -29,7 +25,6 @@ const NavItem = ({ children, title, onClick, disabled = false }: NavItemProps) =
 function TopNavigation() {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
-  const tabs = useAppSelector((state) => state);
 
   function getTabFromUrl() {
     const href = window.location.href;
@@ -37,21 +32,22 @@ function TopNavigation() {
     return path.charAt(0).toUpperCase() + path.slice(1);
   }
 
-  const previosTab = () => {
-    // todo
-  };
-
   const closeTab = () => {
     dispatch(removeTab(getTabFromUrl()));
     navigate('/');
   };
 
   return (
-    <Box component="header" sx={{ height: TOPNAV_HEIGHT }}>
+    <Box
+      component="header"
+      sx={{
+        height: TOPNAV_HEIGHT,
+        position: 'sticky',
+        top: 0,
+        backgroundColor: 'background.default',
+        zIndex: '1',
+      }}>
       <Box component="nav" sx={{ display: 'flex', columnGap: '10px', justifyContent: 'flex-end' }}>
-        <NavItem disabled={tabs.length <= 1} title="Previous" onClick={previosTab}>
-          <IcoBack size={20} />
-        </NavItem>
         <NavItem title="Minimize" onClick={() => navigate('/')}>
           <IcoMinimize size={20} />
         </NavItem>{' '}

bc0987d2b17af6f2d22afb7094e988f1d79f4254
Ivan Kulai <daosonmail@protonmail.com>
2023-03-08 08:37:44 +0200

    Refactored:   - power, sleep, restart buttons replaced with social links   - deleted all relative logic

--- src/Home/StartMenu/Sidebar.tsx
+++ src/Home/StartMenu/Sidebar.tsx
@@ -1,9 +1,39 @@
-import { List } from '@mui/material';
-import StartButton from '../UI/StartMenuButtons';
-import { CiSettings as IcoSetting } from 'react-icons/ci';
-import { BsPower as IcoPower } from 'react-icons/bs';
+import { List, styled, Tooltip, Link } from '@mui/material';
+import {
+  FaTelegramPlane as IcoTg,
+  FaLinkedinIn as IcoIn,
+  FaGithub as IcoGit,
+} from 'react-icons/fa';
 
-function Sidebar({ onClick }: { onClick: () => void }) {
+const SocialLink = styled(Link)({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '64px',
+  padding: '8px 0',
+  color: '#fff',
+  border: '1px solid transparent',
+  '&:hover': {
+    borderRight: '1px solid #4e4d4d',
+    borderTop: '1px solid #757373',
+    backgroundColor: 'rgba(0,0,0, 0.05)',
+  },
+});
+
+function Sidebar() {
+  const linksList = [
+    {
+      title: 'GitHub',
+      icon: <IcoGit size={20} />,
+      link: 'https://github.com/daosongit',
+    },
+    { title: 'Telegram', icon: <IcoTg size={20} />, link: 'https://t.me/bezmn' },
+    {
+      title: 'Linkedin',
+      icon: <IcoIn size={20} />,
+      link: 'https://www.linkedin.com/in/ivan-kulai/',
+    },
+  ];
   return (
     <List
       sx={{
@@ -11,13 +41,15 @@ function Sidebar({ onClick }: { onClick: () => void }) {
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'flex-end',
+        borderRight: '1px solid #3b3a3a',
       }}>
-      <StartButton>
-        <IcoSetting color="white" size={26} />
-      </StartButton>
-      <StartButton onClick={onClick}>
-        <IcoPower color="white" size={23} />
-      </StartButton>
+      {linksList.map((el) => (
+        <Tooltip key={el.title} title={el.title} describeChild placement="right">
+          <SocialLink href={el.link} target="_blank">
+            {el.icon}
+          </SocialLink>
+        </Tooltip>
+      ))}
     </List>
   );
 }
--- src/Home/StartMenu/StartMenu.tsx
+++ src/Home/StartMenu/StartMenu.tsx
@@ -1,18 +1,12 @@
 import { Box } from '@mui/material';
-import { memo, useState } from 'react';
+import { memo } from 'react';
 import PinnedApps from './PinnedApps';
-import PowerPopup from './PowerDetails';
 import Sidebar from './Sidebar';
 
 function StartMenu() {
-  const [powerPopup, setPowerPopup] = useState<boolean>(false);
-  const handleOpen = () => {
-    setPowerPopup(!powerPopup);
-  };
-
   return (
     <>
-      <Sidebar onClick={handleOpen} />
+      <Sidebar />
       <Box
         sx={{
           width: '100%',
@@ -22,7 +16,6 @@ function StartMenu() {
           justifyContent: 'space-between',
         }}>
         <PinnedApps />
-        <PowerPopup power={powerPopup} />
       </Box>
     </>
   );
